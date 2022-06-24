4f9cc4c193065e4d6dc81c6eea22acc1a068888c
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-06-24 14:48:59 +0200

    added search movies by input value

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const searchForm = document.querySelector('#search');
 const searchInput = document.querySelector('input');
 
-searchForm.addEventListener('submit', (e) => {
+searchForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
-	const movies = getMovies();
-	const search = searchInput.value;
-	console.log(search);
+	const movies = await getMovies();
+	const search = searchInput.value.toLowerCase();
+	movies.forEach((movie) => {
+		const movieName = movie.name.toLowerCase();
+		if (movieName.includes(search)) {
+			console.log(movie);
+		}
+	});
+	console.log('helloe');
 });
 
 const getMovies = async () => {
-	const res = await axios.get('https://api.tvmaze.com/shows');
-	const movies = res.data;
-	return movies;
+	try {
+		const res = await axios.get('https://api.tvmaze.com/shows');
+		const movies = res.data;
+		return movies;
+	} catch (error) {
+		console.log('error: ', error);
+	}
+};
+
+const createImage = (movie) => {
+	const image = document.createElement('img');
 };
